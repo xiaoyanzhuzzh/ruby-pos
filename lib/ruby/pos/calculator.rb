17473a3b90b4a4dea4fb28eb5844f4d68522e86d
zhzhang <zhzhang@thoughtworks.com>
2016-04-15 17:26:34 +0800

    Calculator class: calculate subtotal for buy_two_get_one promotion.

--- lib/ruby/pos/calculator.rb
+++ lib/ruby/pos/calculator.rb
@@ -1,2 +1,7 @@
 class Calculator
+  def self.calculate_buy_two_get_one cart_item
+    promotion_number = cart_item.number / 3
+    actual_number = cart_item.number - promotion_number
+    return actual_number * cart_item.item_price
+  end
 end
--- spec/ruby/pos/calculator_spec.rb
+++ spec/ruby/pos/calculator_spec.rb
@@ -1,41 +1,18 @@
 require 'rspec'
-require 'ruby/pos/promotion_calculator'
+require 'ruby/pos/calculator'
+require 'ruby/pos/models/item'
+require 'ruby/pos/models/cart_item'
 
+describe Calculator do
 
-describe Printer do
+  let(:apple) { Item.new('ITEM000001', 'apple', 3.0, '个', 'buy_two_get_one') }
+  let(:cart_apple) { CartItem.new(apple, 3) }
 
-  subject(:printer) { Printer.new }
-  let(:item) { Item.new('ITEM000001', 'apple', 3.0, '个', '0') }
-  let(:item1) { Item.new('ITEM000002', 'watermelon', 1.0, '斤', '0') }
-  let(:cart_item_list) { [CartItem.new(item, 3)] }
-  let(:cart_item_list1) { [CartItem.new(item, 3), CartItem.new(item1, 13)] }
+  describe '#calculate_buy_two_get_one' do
 
-  describe '#InvoiceDetails' do
-
-    it 'should print one cart item details when give one cart item' do
-      subject.register_cart_item_list cart_item_list
-
-      details = printer.invoice_details
-      expect(details).to eq("名称: apple, 数量: 3个, 单价: 3.00(元), 小计: 9.00(元)\n")
-    end
-
-    it 'should print two cart items details when give two cart items' do
-      subject.register_cart_item_list cart_item_list1
-
-      details = printer.invoice_details
-      expect(details).to eq("名称: apple, 数量: 3个, 单价: 3.00(元), 小计: 9.00(元)\n" +
-                                "名称: watermelon, 数量: 13斤, 单价: 1.00(元), 小计: 13.00(元)\n")
+    it 'should calculate correct subtotal when given a buy_two_get_one cart item' do
+      subtotal = Calculator.calculate_buy_two_get_one cart_apple
+      expect(subtotal).to eq(6.00)
     end
   end
-
-  describe '#invoiceTotal' do
-
-    it 'should calculate the total price' do
-      subject.register_cart_item_list cart_item_list1
-
-      total_price = printer.invoice_total
-      expect(total_price).to eq('总价: 22.00(元)')
-    end
-  end
-
 end
