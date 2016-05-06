e51c428ab006415069bde26f4ee621df81673f2d
zhzhang <zhzhang@thoughtworks.com>
2016-05-06 16:37:45 +0800

    refactor codes.

--- lib/ruby/pos/printer.rb
+++ lib/ruby/pos/printer.rb
@@ -15,22 +15,7 @@ class Printer
   end
 
   def invoice_details
-    cart_items_details = ''
-    @cart_item_list.each do |cart_item|
-      if cart_item.item_promotion.eql?(0.95)
-        cart_items_details << "名称: #{cart_item.item_name}, 数量: #{cart_item.number}#{cart_item.item_unit}, 单价: #{format_price(cart_item.item_price)}(元), 小计: #{format_price(cart_item.subtotal)}(元), 节省: #{format_price(cart_item.item_price * cart_item.number - cart_item.subtotal)}(元)\n"
-      else
-        cart_items_details << "名称: #{cart_item.item_name}, 数量: #{cart_item.number}#{cart_item.item_unit}, 单价: #{format_price(cart_item.item_price)}(元), 小计: #{format_price(cart_item.subtotal)}(元)\n"
-      end
-    end
-
-    buy_two_get_one_cart_item_list = @cart_item_list.select { |each| each.item_promotion.eql?('buy_two_get_one') }
-    cart_items_details << "----------------------------------\n买二赠一商品:\n" unless buy_two_get_one_cart_item_list.empty?
-    buy_two_get_one_cart_item_list.each do |each_cart_item|
-      cart_items_details << "名称: #{each_cart_item.item_name}, 数量: #{each_cart_item.number / 3}#{each_cart_item.item_unit}\n"
-    end
-
-    cart_items_details
+    get_items_details << get_promotion_details
   end
 
   def invoice_total
@@ -49,7 +34,33 @@ class Printer
     '**********************'
   end
 
+  private
   def format_price price
     format('%.2f', price)
   end
+
+  private
+  def get_items_details
+    item_details = ''
+    @cart_item_list.each do |cart_item|
+      if cart_item.item_promotion.eql?(0.95)
+        item_details << "名称: #{cart_item.item_name}, 数量: #{cart_item.number}#{cart_item.item_unit}, 单价: #{format_price(cart_item.item_price)}(元), 小计: #{format_price(cart_item.subtotal)}(元), 节省: #{format_price(cart_item.item_price * cart_item.number - cart_item.subtotal)}(元)\n"
+      else
+        item_details << "名称: #{cart_item.item_name}, 数量: #{cart_item.number}#{cart_item.item_unit}, 单价: #{format_price(cart_item.item_price)}(元), 小计: #{format_price(cart_item.subtotal)}(元)\n"
+      end
+    end
+    item_details
+  end
+
+  private
+  def get_promotion_details
+    promotion_details = ''
+    buy_two_get_one_cart_item_list = @cart_item_list.select { |each| each.item_promotion.eql?('buy_two_get_one') }
+    promotion_details << "----------------------------------\n买二赠一商品:\n" unless buy_two_get_one_cart_item_list.empty?
+    buy_two_get_one_cart_item_list.each do |each_cart_item|
+      promotion_details << "名称: #{each_cart_item.item_name}, 数量: #{each_cart_item.number / 3}#{each_cart_item.item_unit}\n"
+    end
+    promotion_details
+  end
+
 end
